80846f496aabac25f39a63734d5adc6882742acf
Akomea Kenn <kakomea@yahoo.com>
2017-09-05 03:41:40 +0800

    implement cardView and add resource files

--- app/src/main/java/ghsoft/yenhyia_chatmeetdating/HomeFragment.java
+++ app/src/main/java/ghsoft/yenhyia_chatmeetdating/HomeFragment.java
@@ -7,8 +7,20 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import link.fls.swipestack.SwipeStack;
 
 public class HomeFragment extends Fragment {
+    private SwipeStack cardStack;
+    private CardsAdapter cardsAdapter;
+    private ArrayList<CardItem> cardItems;
+    private View btnCancel;
+    private View btnLove;
+    private int currentPosition;
+
     public static HomeFragment newInstance() {
         HomeFragment homeFragment = new HomeFragment();
         return homeFragment;
@@ -18,6 +30,7 @@ public class HomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
     }
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
@@ -29,8 +42,64 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-
         getActivity().findViewById(R.id.my_toolbar).setVisibility(View.VISIBLE);
+
+        cardStack = v.findViewById(R.id.card_container);
+        btnCancel = v.findViewById(R.id.cancel);
+        btnLove = v.findViewById(R.id.love);
+
+        setCardStackAdapter();
+        currentPosition = 0;
+
+        //Handling swipe event of Cards stack
+        cardStack.setListener(new SwipeStack.SwipeStackListener() {
+            @Override
+            public void onViewSwipedToLeft(int position) {
+                currentPosition = position + 1;
+            }
+
+            @Override
+            public void onViewSwipedToRight(int position) {
+                currentPosition = position + 1;
+            }
+
+            @Override
+            public void onStackEmpty() {
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardStack.swipeTopViewToRight();
+            }
+        });
+
+        btnLove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "You liked " + cardItems.get(currentPosition).getName(),
+                        Toast.LENGTH_SHORT).show();
+                cardStack.swipeTopViewToLeft();
+            }
+        });
         return v;
     }
+    private void setCardStackAdapter() {
+        cardItems = new ArrayList<>();
+
+        cardItems.add(new CardItem(R.drawable.a, "Angela", "Hottie"));
+        cardItems.add(new CardItem(R.drawable.f, "Selena", "Gomez"));
+        cardItems.add(new CardItem(R.drawable.g, "Dorothy", "Amy"));
+        cardItems.add(new CardItem(R.drawable.e, "Lyanna", "Stark"));
+        cardItems.add(new CardItem(R.drawable.c, "Lydia", "Williewetty"));
+        cardItems.add(new CardItem(R.drawable.d, "Diamond", "Waters"));
+        cardItems.add(new CardItem(R.drawable.b, "Liana", "Ying"));
+
+        cardsAdapter = new CardsAdapter(getActivity(), cardItems);
+        cardStack.setAdapter(cardsAdapter);
+    }
+
+
 }
